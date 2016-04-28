c4d79777ee63ba84b1e8676a2c7794b11ac6fcdf
Kathryn Hodge <kahodge@vassar.edu>
2016-04-28 14:21:42 -0400

    Problems with ng-route

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,3 +1,26 @@
-app.controller('HomeController', ['$scope', function($scope) {
-	$scope.helloWorld = "Hello, AngularJS!";
+app.controller('HomeController', ['$scope', 'suggestions', function($scope, suggestions) {
+	$scope.posts = suggestions.posts;
+
+	$scope.addSuggestion = function() {
+		// if input empty, don't submit
+		if (!$scope.title || $scope.title == "") {
+			return
+		}
+
+		// push suggestion posts in suggestions.js
+		$scope.posts.push({
+			title: $scope.title,
+			upvotes:0
+			comments:[]
+		})
+		
+		// after submit, clear input
+		$scope.title = "";
+		
+	};
+
+	$scope.upVote = function(post) {
+		post.upvotes+=1;
+	};
+
 }]);
